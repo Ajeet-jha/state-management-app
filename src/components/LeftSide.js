ef5d3f4f5a176ead7fc47e62531198b4c9c2f547
Ajeet jha <jhaajeetcs@gmail.com>
2022-10-26 14:34:57 +0530

    Added fetch user in left side componet

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
+import http from '../services/httpConfig';
+
 function LeftSide() {
-	return <section className="left-side">LeftSide</section>;
+	const [select, setSelect] = useState(-1);
+	const [users, setUsers] = useState([]);
+	const [user, setUser] = useState([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const resp = await http.get('/users');
+			setUsers(resp.data);
+		};
+		fetchUsers();
+	}, []);
+
+	const fetchUser = async (id) => {
+		const userResp = await http.get(`/users/${id}`);
+		setUser(userResp.data);
+	};
+
+	const handleSelect = (e) => {
+		const selected = e.target.value;
+		setSelect(selected);
+		if (selected > 0) {
+			fetchUser(selected);
+		} else {
+			setUser([]);
+		}
+	};
+
+	return (
+		<section className="left-side">
+			<label htmlFor="select-user">
+				<select id="select-user" value={select} onChange={handleSelect}>
+					<option value={-1}>Choose user</option>
+					{users.map(({ id, name }) => (
+						<option key={id} value={id}>
+							{name}
+						</option>
+					))}
+				</select>
+			</label>
+			<pre>{JSON.stringify(user, null, 2)}</pre>
+		</section>
+	);
 }
 
 export default LeftSide;
