02ff4d2fc22e3e820830ccc82b32533145dc0d9c
Ajeet jha <jhaajeetcs@gmail.com>
2022-10-26 15:49:53 +0530

    Added delete request in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+import http from '../services/httpConfig';
+
 function Footer() {
-	return <footer className="footer">Footer</footer>;
+	const [select, setSelect] = useState(-1);
+	const [users, setUsers] = useState([]);
+
+	const fetchUsers = async () => {
+		const resp = await http.get('/users');
+		setUsers(resp.data);
+	};
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	const handleSelect = async (e) => {
+		const selected = e.target.value;
+		setSelect(selected);
+		if (selected > 0) {
+			await http.delete(`/users/${selected}`);
+		}
+	};
+
+	return (
+		<footer className="footer">
+			<label htmlFor="select-user">
+				<select id="select-user" value={select} onChange={handleSelect}>
+					<option value={-1}>Choose user</option>
+					{users.map(({ id, name }) => (
+						<option key={id} value={id}>
+							{name}
+						</option>
+					))}
+				</select>
+			</label>
+		</footer>
+	);
 }
 
 export default Footer;
