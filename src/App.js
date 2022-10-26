7c66c991f610724c523b7a8a2db2be844f15f50e
Ajeet jha <jhaajeetcs@gmail.com>
2022-10-26 16:50:36 +0530

    Moved left side component states to app component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react';
+import { fetchUsers, fetchUser } from './services/Api';
+
 import * as Components from './components';
 
 function App() {
+	const [select, setSelect] = useState(-1);
+	const [users, setUsers] = useState([]);
+	const [user, setUser] = useState([]);
+
+	useEffect(() => {
+		const getAllUsers = async () => {
+			const resp = await fetchUsers({
+				url: '/users',
+				method: 'GET',
+			});
+			setUsers(resp);
+		};
+		getAllUsers();
+	}, []);
+
+	const findUser = async (id) => {
+		const getUser = async () => {
+			const userResp = await fetchUser({
+				url: `/users/${id}`,
+				method: 'GET',
+			});
+			setUser(userResp);
+		};
+		getUser();
+	};
+
 	return (
 		<section>
 			<Components.Header />
-			<Components.LeftSide />
+			<Components.LeftSide
+				select={select}
+				setSelect={setSelect}
+				user={user}
+				setUser={setUser}
+				users={users}
+				setUsers={setUsers}
+				findUser={findUser}
+			/>
 			<Components.Main />
 			<Components.RightSide />
 			<Components.Footer />
--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,33 +1,4 @@
-import { useEffect, useState } from 'react';
-import { fetchUsers, fetchUser } from '../services/Api';
-
-function LeftSide() {
-	const [select, setSelect] = useState(-1);
-	const [users, setUsers] = useState([]);
-	const [user, setUser] = useState([]);
-
-	useEffect(() => {
-		const getAllUsers = async () => {
-			const resp = await fetchUsers({
-				url: '/users',
-				method: 'GET',
-			});
-			setUsers(resp);
-		};
-		getAllUsers();
-	}, []);
-
-	const findUser = async (id) => {
-		const getUser = async () => {
-			const userResp = await fetchUser({
-				url: `/users/${id}`,
-				method: 'GET',
-			});
-			setUser(userResp);
-		};
-		getUser();
-	};
-
+function LeftSide({ select, setSelect, users, findUser, user, setUser }) {
 	const handleSelect = (e) => {
 		const selected = e.target.value;
 		setSelect(selected);
