4cc1c172d8f9fa3e1379d3b7ce1fca3a2d2c0105
Ajeet jha <jhaajeetcs@gmail.com>
2022-10-27 13:35:21 +0530

    Added redux dev-tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-underscore-dangle */
+
 import { createStore } from 'redux';
 import { rootReducer } from './reducers/rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
